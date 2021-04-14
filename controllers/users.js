885f140c26d9f082357c6f4338f602c744f602c3
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-14 15:28:39 +0300

    add user controller

--- controllers/users.js
+++ controllers/users.js
@@ -21,3 +21,29 @@ const getUserInfo = (req, res, next) => {
       }
     });
 };
+
+const updateProfile = (req, res, next) => {
+  const { email, name } = req.body;
+  UserModel.findByIdAndUpdate(req.user._id, { $set: { email, name } }, {
+    runValidators: true,
+    new: true,
+  })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь с данным id не найден');
+      } else {
+        res.status(200).send({ user });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        throw new BadRequestError('Переданы некорректные данные!');
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = {
+  getUserInfo, updateProfile
+}
