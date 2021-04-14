36df47012d555338811f0a2f30125de5556b7401
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-14 15:35:30 +0300

    add config to app.js

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const app = express();
+
+const usersRouter = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+});
+
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', usersRouter);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({ message: statusCode === 500 ? 'Ошибка сервера!' : message });
+
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
