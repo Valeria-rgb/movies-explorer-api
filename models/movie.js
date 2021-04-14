0b6901847e45044aacfcdc1f1dff38867da7d542
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-14 11:47:50 +0300

    add movie schema and model

---
+++ models/movie.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const { urlRegex } = require('../utils/constants')
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => {
+        return urlRegex.test(url);
+      },
+      message: (props) => `${props.value} поле 'image' должно быть валидным url-адресом!`,
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => {
+        return urlRegex.test(url);
+      },
+      message: (props) => `${props.value} поле 'trailer' должно быть валидным url-адресом!`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => {
+        return urlRegex.test(url);
+      },
+      message: (props) => `${props.value} поле 'thumbnail' должно быть валидным url-адресом!`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
+
+
