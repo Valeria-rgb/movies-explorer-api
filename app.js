4026a46dcd2f65abc8bf3aee6c1d47cd4e21ffbc
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-14 09:16:20 +0300

    add user schema and model

---
+++ app.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
