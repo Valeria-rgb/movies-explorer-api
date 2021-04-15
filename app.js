04c1923e5b30d1945b6c9bc55bb62d302a461b8d
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-15 15:11:53 +0300

    add logger

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-const indexRouter = require('./routes/index');
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
@@ -19,7 +20,11 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(requestLogger);
+
 app.use('/', indexRouter);
+
+app.use(errorLogger);
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
