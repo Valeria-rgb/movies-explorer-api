7d9a21a5c1171529ea90ed406f18e7fb868cfabc
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-16 12:48:17 +0300

    change dependencies

--- utils/config.js
+++ utils/config.js
@@ -3,14 +3,14 @@ const rateLimit = require('express-rate-limit');
 const MongoUrl = 'mongodb://localhost:27017/bitfilmsdb';
 const MongoConfig = {
   useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 };
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100
+  max: 100,
 });
 
 module.exports = { MongoUrl, MongoConfig, limiter };
